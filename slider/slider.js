6826f16916bf9b72e40278bef208cc9d12e3b7e4
Himanshu Garg <114793931+Hima93511@users.noreply.github.com>
2024-02-09 13:21:44 +0530

    Update slider.js

--- slider/slider.js
+++ slider/slider.js
@@ -1,2 +1,26 @@
 const a=document.querySelectorAll('.slider');
-console.log(a);
+var counter=0;
+// console.log(a);
+a.forEach(
+    (slider ,index) =>
+    {
+        slider.style.top= `${index*100}%`
+    }
+)
+const Previous=()=>
+{
+    counter--
+    sliderrun()
+}
+const Next=()=>
+{
+    counter++
+    sliderrun()
+}
+const sliderrun = () => {
+    a.forEach(
+        (slider) => {
+            slider.style.transform = `translateY(-${counter * 100}%)`
+        }
+    )
+}
